674b44089f28cd0662ac63ecefb0c2765f6172cb
Bulat Kurbanov <kurbanovbul@gmail.com>
2024-08-09 22:36:37 +0200

    Fix starting

--- src/main.py
+++ src/main.py
@@ -3,9 +3,16 @@ from asyncio import gather
 from services.discord import start_discord_sevice
 from services.twitch import start_twitch_service
 
+
 async def main():
     print("Starting services...")
     await gather(
         start_discord_sevice(),
         start_twitch_service()
     )
+
+
+if __name__ == "__main__":
+    from asyncio import run
+
+    run(main())
