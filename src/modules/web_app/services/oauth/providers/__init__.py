c54c6aa9c41f250d52cc0d158ce48927aaab2baf
Bulat Kurbanov <kurbanovbul@gmail.com>
2024-11-20 00:30:20 +0100

    Fix

--- src/modules/web_app/services/oauth/providers/__init__.py
+++ src/modules/web_app/services/oauth/providers/__init__.py
@@ -1,10 +1,8 @@
-from .enums import OAuthProvider
 from .twitch import twitch_oauth_client
 from .getter import get_client
 
 
 __all__ = [
-    "OAuthProvider",
     "twitch_oauth_client",
     "get_client"
 ]
