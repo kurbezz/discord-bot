f1630cd6960914f0d9d8f6fcfb309381f1c3e88b
Bulat Kurbanov <kurbanovbul@gmail.com>
2024-11-19 15:45:08 +0100

    Fix

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,10 +4,4 @@ response=`curl -X 'GET' "https://$VAULT_HOST/v1/$VAULT_SECRET_PATH" -s \
   -H 'accept: application/json' \
   -H "X-Vault-Token: $VAULT_TOKEN"`
 
-data=`echo $response | jq -r '.data.data'`
-
-echo "$data" | jq -r 'to_entries[] | "\(.key)=\(.value)"' | while IFS= read -r line; do
-    key=$(echo "$line" | cut -d '=' -f 1)
-    value=$(echo "$line" | cut -d '=' -f 2-)
-    echo "$key=\"$value\""
-done
+echo "$(echo "$response" | jq -r '.data.data | to_entries | map("\(.key)=\(.value)") | .[]')"
